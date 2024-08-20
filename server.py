301e126a9f799d2794ed7c924fca09707c0a2c6f
Kevin Power <kevinp3099@gmail.com>
2024-08-20 19:38:21 +0100

    Adding the GET and PUT methods in

--- server.py
+++ server.py
@@ -1,8 +1,8 @@
-from flask import Flask,render_template_string
+from flask import Flask,render_template_string,jsonify, request
 
 app = Flask(__name__)
 
-html_template = '>h1> Hello World </h1>'
+html_template = '<h1> Hello World </h1>'
 
 # In-Memory Storage for the key-value pairs
 store = {}
@@ -13,15 +13,26 @@ def index():
 
 @app.route('/store/<key>', methods=['GET'])
 def get_value(key):
-    return render_template_string(html_template)
+    value = store.get(key)
+    if value is None:
+        return jsonify("No Key Value Store found"), 404
+    return jsonify({key: value})
 
 @app.route('/store/<key>', methods=['PUT'])
-def get_value(key):
-    return render_template_string(html_template)
+def set_value(key):
+    value = request.json.get('value')
+    if value is None:
+        return jsonify({"Error: No value provided"}), 400
+    store[key] = value
+    return jsonify({key:value}), 201
 
-@app.route('/store/<key>', methods=['DELETE'])
-def get_value(key):
-    return render_template_string(html_template)
+    
+    
+
+
+#@app.route('/store/<key>', methods=['DELETE'])
+#def delete_value(key):
+#    return render_template_string(html_template)
 
 if __name__ == '__main__':
     app.run(port=8000)
